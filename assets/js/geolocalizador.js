b6925bc97ba2def8794d71d5e767c0bf7b04f64c
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-07 09:53:43 -0400

    JJ_Pedrique:Intento 8

--- assets/js/geolocalizador.js
+++ assets/js/geolocalizador.js
@@ -22,7 +22,7 @@ function obtenerUbicacion() {
 
 // Se ejecuta si el usuario acepta y se obtiene la ubicación
 function exito(posicion) {
-    const SandBox = document.getElementById("");
+    const SandBox = document.getElementById("SandBox");
     const latitud = posicion.coords.latitude;
     const longitud = posicion.coords.longitude;
     const precision = posicion.coords.accuracy; // Precisión en metros
