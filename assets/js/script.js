a60aba1e1de4aea7d263f0fa6cb6684f472537db
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-17 22:52:44 -0400

    JJ_Pedrique: 10:52pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ window.onload = () => {
       const entity = document.createElement('a-box');
       entity.setAttribute('material', 'color: #E74C3C');
       entity.setAttribute('scale', '1 1 1');
-      entity.setAttribute('gps-entity-place', `latitude: ${latitude-0.001}; longitude: ${longitude-0.001};`);
+      entity.setAttribute('gps-entity-place', `latitude: ${latitude - 0.0001}; longitude: ${longitude - 0.0001};`);
 
       scene.appendChild(entity);
     },
