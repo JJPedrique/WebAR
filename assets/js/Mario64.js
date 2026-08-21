33ebfcd12e26ece6ab5bedb27dbafb4621477f1e
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-21 11:39:05 -0400

    JJ_Pedrique: try5

--- assets/js/Mario64.js
+++ assets/js/Mario64.js
@@ -5,28 +5,25 @@ window.onload = () => {
 
         const scene = document.querySelector('a-scene');
 
-        const entity = document.createElement('a-entity');
-        scene.appendChild(entity);
-        entity.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
+        const entity = document.createElement('a-entity'); 
+        entity.setAttribute('gps-entity-place', `latitude: ${latitude - 0.00006}; longitude: ${longitude};`);
         entity.setAttribute('gltf-model','assets/3Dmodels/Mario64/scene.gltf');
         entity.setAttribute('scale','5 5 5');
         entity.setAttribute('look-at','[gps-camera]');
-       
+        scene.appendChild(entity);       
         
         const text = document.createElement('a-text');
-        scene.appendChild(text);
-        text.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
+        text.setAttribute('gps-entity-place', `latitude: ${latitude - 0.00006}; longitude: ${longitude};`);
         text.setAttribute('value',"¡It's-a me, Mario!");
         text.setAttribute('scale','1 1 1');
+        text.setAttribute('position','-1 1 0');
         text.setAttribute('look-at','[gps-camera]');
-        
+        scene.appendChild(text);        
    
         let Lat =  document.getElementById('latitude');
         let Lon =  document.getElementById('longitude');
-
         Lat.innerHTML=`Latitude: ${latitude}`;
         Lon.innerHTML=`Longitude: ${longitude}`;
-
 
         },
         (err) => console.error('Error al obtener la geolocalización:', err),
