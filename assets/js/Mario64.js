6ab5d4c932e8cfcb4fe8506f59c0d1e1e8776219
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-21 12:25:03 -0400

    JJ_Pedrique: final idk

--- assets/js/Mario64.js
+++ assets/js/Mario64.js
@@ -12,14 +12,6 @@ window.onload = () => {
         entity.setAttribute('look-at','[gps-camera]');
         scene.appendChild(entity);       
         
-        const text = document.createElement('a-text');
-        text.setAttribute('gps-entity-place', `latitude: ${latitude - 0.00006}; longitude: ${longitude};`);
-        text.setAttribute('value',"¡It's-a me, Mario!");
-        text.setAttribute('scale','1 1 1');
-        //text.setAttribute('position','-0.5 0.5 0');
-        text.setAttribute('look-at','[gps-camera]');
-        scene.appendChild(text);        
-   
         let Lat =  document.getElementById('latitude');
         let Lon =  document.getElementById('longitude');
         Lat.innerHTML=`Latitude: ${latitude}`;
