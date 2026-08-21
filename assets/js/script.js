106e1a9e213406059cf6a552e5183bb9ab7efd8a
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-21 12:37:07 -0400

    JJ_Pedrique: final4

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,8 @@ window.onload = () => {
         entity.setAttribute('look-at','[gps-camera]');
         scene.appendChild(entity);       
         
-        let Lat =  document.getElementById('latitude');
-        let Lon =  document.getElementById('longitude');
-        Lat.innerHTML=`Latitude: ${latitude}`;
-        Lon.innerHTML=`Longitude: ${longitude}`;
-
+        let info =  document.getElementById('info');
+        info.innerHTML=`Latitude: ${latitude}    Longitude: ${longitude}`;
         },
         (err) => console.error('Error al obtener la geolocalización:', err),
         { enableHighAccuracy: true }
