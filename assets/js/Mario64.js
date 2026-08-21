cc38c0fa54ddb70bdda8e278c8bc8e63eed1a96b
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-21 10:15:22 -0400

    JJ_Pedrique: 10:15am

--- assets/js/Mario64.js
+++ assets/js/Mario64.js
@@ -6,18 +6,20 @@ window.onload = () => {
         const scene = document.querySelector('a-scene');
 
         const entity = document.createElement('a-entity');
+        scene.appendChild(entity);
         entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         entity.setAttribute('gltf-model','assets/3Dmodels/Mario64/scene.gltf');
         entity.setAttribute('scale','5 5 5');
         entity.setAttribute('look-at','[gps-camera]');
-        scene.appendChild(entity);
+       
         
         const text = document.createElement('a-text');
+        scene.appendChild(text);
         text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         text.setAttribute('value',"¡It's-a me, Mario!");
         text.setAttribute('scale','1 1 1');
         text.setAttribute('look-at','[gps-camera]');
-        scene.appendChild(text);
+        
 
         },
         (err) => console.error('Error al obtener la geolocalización:', err),
