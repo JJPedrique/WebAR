f075e0d44664f413ca9e264b60ed0dddcef9f782
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-21 11:28:54 -0400

    JJ_Pedrique: try3

--- assets/js/Mario64.js
+++ assets/js/Mario64.js
@@ -1,4 +1,4 @@
-/*window.onload = () => {
+window.onload = () => {
     navigator.geolocation.getCurrentPosition((position) => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
@@ -7,7 +7,7 @@
 
         const entity = document.createElement('a-entity');
         scene.appendChild(entity);
-        entity.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
+        //entity.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
         entity.setAttribute('gltf-model','assets/3Dmodels/Mario64/scene.gltf');
         entity.setAttribute('scale','5 5 5');
         entity.setAttribute('look-at','[gps-camera]');
@@ -15,7 +15,7 @@
         
         const text = document.createElement('a-text');
         scene.appendChild(text);
-        text.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
+        //text.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
         text.setAttribute('value',"¡It's-a me, Mario!");
         text.setAttribute('scale','1 1 1');
         text.setAttribute('look-at','[gps-camera]');
@@ -32,42 +32,4 @@
         (err) => console.error('Error al obtener la geolocalización:', err),
         { enableHighAccuracy: true }
     );
-};*/
-
-window.addEventListener('gps-camera-update-positon',(e) =>{
-    const { latitude, longitude } = e.detail.position;
-
-    const scene = document.querySelector('a-scene');
-
-    const entity = document.createElement('a-entity');
-    scene.appendChild(entity);
-    entity.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
-    entity.setAttribute('gltf-model','assets/3Dmodels/Mario64/scene.gltf');
-    entity.setAttribute('scale','5 5 5');
-    entity.setAttribute('look-at','[gps-camera]');
-    
-    
-    const text = document.createElement('a-text');
-    scene.appendChild(text);
-    text.setAttribute('gps-entity-place', `latitude: ${latitude - 0.000065}; longitude: ${longitude};`);
-    text.setAttribute('value',"¡It's-a me, Mario!");
-    text.setAttribute('scale','1 1 1');
-    text.setAttribute('look-at','[gps-camera]');
-    
-
-
-
-
-
-
-
-
-
-
-
-    
-    let Lat =  document.getElementById('latitude');
-    let Lon =  document.getElementById('longitude');
-    Lat.innerHTML=`Latitude: ${latitude}`;
-    Lon.innerHTML=`Longitude: ${longitude}`;
-});
+};
